6128bc52dc7091ae93242dbf97dbb27d8d9e9907
m_hammad-ur-rehman <hammadurrehman7707@gmail.com>
2024-10-29 11:19:08 +0500

    Header Footer Added

---
+++ project-1-countdown_timer/app/page.tsx
@@ -0,0 +1,10 @@
+import Countdown from "@/components/ui/count-down";
+import Footer from "@/components/ui/Footer";
+import Navbar from "@/components/ui/Navbar";
+export default function Home() {
+  return (<>
+    <Navbar />
+    <Countdown />
+    <Footer />
+  </>);
+}
