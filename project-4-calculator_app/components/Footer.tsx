3fd9e2a872b86e7642e24ffb90991ce82f3d08d1
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2024-12-27 12:23:09 +0500

    Create project-4-calculator_app

---
+++ project-4-calculator_app/components/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <div>
+            <footer className="footer footer-center bg-slate-800 text-white p-4">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved.</p>
+                </aside>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
