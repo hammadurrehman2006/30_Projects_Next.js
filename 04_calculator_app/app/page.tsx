b50580325b3790eeafa3432b7c71f6199c2d646d
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2025-03-16 16:06:36 +0500

    04_calculator_app Added

---
+++ 04_calculator_app/app/page.tsx
@@ -0,0 +1,13 @@
+import Calculator from "@/components/Calculator";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Calculator/>
+    <Footer/>
+    </>
+  );
+}
